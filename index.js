005ce3f19d34827b8a57f24ced3e679465ceb3d0
Reginald Mullin <ramumb@yahoo.com>
2019-07-08 00:29:20 -0400

    Create index.js
    Initial version

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * This is a port of the PrototypeJS method stripTags.
+ * It Strips a string of any HTML tags.
+ * @param {string}  A string from which to strip HTML tags
+ * @return {string} The result of the function
+ */
+
+function stripTags(str) {
+    return String(str).replace(/<\w+(\s+("[^"]*"|'[^']*'|[^>])+)?(\/)?>|<\/\w+>/gi, '');;
+}
+
+module.exports = stripTags;
